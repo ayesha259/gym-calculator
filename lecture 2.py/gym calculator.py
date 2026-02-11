fedcd3f3f911522228bb45ce6f565b80cf1c38e7
Your Name <you@example.com>
2026-02-11 01:35:16 -0800

    Fix syntax error in gym calculator.py

--- lecture 2.py/gym calculator.py
+++ lecture 2.py/gym calculator.py
@@ -2,7 +2,7 @@ registration_fee = float(input("Enter registration fee: "))
 monthly_fee = float(input("Enter monthly fee: "))
 months = int(input("Enter number of months to pay for: "))
 
-total_cost = registration_fee + (monthly_fee * months)5000
+total_cost = registration_fee + (monthly_fee * months)
 
 coupon = input("Do you have a discount coupon? (yes/no): ").lower()
 
